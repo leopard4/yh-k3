8b70973effb51df39063e97c1e5957cb27f6e4ed
blockenters <58394003+blockenters@users.noreply.github.com>
2022-12-13 16:45:56 +0900

    Add files via upload

--- streamlit/app_eda.py
+++ streamlit/app_eda.py
@@ -23,16 +23,41 @@ def run_eda_app() :
 
     seleced_column = st.selectbox('컬럼을 선택하세요.', column_list)
 
-    df.loc[ df[seleced_column] ==  df[seleced_column].max() , ]
-    df.loc[ df[seleced_column] ==  df[seleced_column].min() , ]
+    df_max = df.loc[ df[seleced_column] ==  df[seleced_column].max() , ]
+    df_min = df.loc[ df[seleced_column] ==  df[seleced_column].min() , ]
+
+    st.text('최대 데이터')
+    st.dataframe(df_max)
+    st.text('최소 데이터')
+    st.dataframe(df_min)
+
+
+    st.subheader('컬럼 별 히스토그램')
+
+    histogram_column = st.selectbox('히스토그램 확인할 컬럼을 선택하세요.', column_list)
+    my_bins = st.number_input('빈의 갯수를 입력하세요', 10, 30, value=10, step=1)
+
+    fig1 = plt.figure()
+    plt.hist(data= df, x=histogram_column, rwidth=0.8, bins=my_bins)
+    plt.title(histogram_column + ' Histogram')
+    plt.xlabel(histogram_column)
+    plt.ylabel('Count')
+    st.pyplot(fig1)
 
 
 
+    st.subheader('상관 관계 분석')
 
+    selected_list = st.multiselect('상관분석을 하고싶은 컬럼을 선택하세요', column_list)
 
+    if len(selected_list) >= 2 :
 
+        df_corr = df[ selected_list ].corr()
 
-
+        fig2 = plt.figure()
+        sb.heatmap(data= df_corr, annot=True, fmt='.2f', cmap='coolwarm',
+            vmin= -1, vmax= 1 , linewidths=0.5 )
+        st.pyplot(fig2)
 
 
 
