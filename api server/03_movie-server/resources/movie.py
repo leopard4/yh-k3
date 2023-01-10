d3e8f33410d1a226269986ae50d430f487521ce7
blockenters <blockenters@gmail.com>
2023-01-10 13:06:37 +0900

    Update movie.py

--- api server/03_movie-server/resources/movie.py
+++ api server/03_movie-server/resources/movie.py
@@ -8,8 +8,14 @@ from flask_jwt_extended import get_jwt_identity
 from mysql_connection import get_connection
 
 class MovieListResource(Resource) :
-
+    
+    @jwt_required(optional=True)
     def get(self) :
+
+        user_id = get_jwt_identity()
+
+        print("유저 아이디")
+        print(user_id)
 
         order = request.args.get('order')
         offset = request.args.get('offset')
@@ -18,21 +24,38 @@ class MovieListResource(Resource) :
         try :
             connection = get_connection()
 
-            query = '''select m.id, m.title, 
-                    ifnull(count(r.movie_id), 0) as cnt , 
-                    ifnull(avg(r.rating) , 0)  as avg
-                    from movie m 
-                    left join rating r
-                    on m.id = r.movie_id
-                    group by m.id
-                    order by '''+ order +''' desc
-                    limit '''+ offset +''' , '''+ limit +''' ;'''
+            if user_id is None :
 
-            # record = (user_id, )
+                query = '''select m.id, m.title, 
+                        ifnull(count(r.movie_id), 0) as cnt , 
+                        ifnull(avg(r.rating) , 0)  as avg
+                        from movie m 
+                        left join rating r
+                        on m.id = r.movie_id
+                        group by m.id
+                        order by '''+ order +''' desc
+                        limit '''+ offset +''' , '''+ limit +''' ;'''
 
-            cursor = connection.cursor(dictionary=True)
+                cursor = connection.cursor(dictionary=True)
+                cursor.execute(query)
 
-            cursor.execute(query)
+            else : 
+                query = '''select m.id, m.title,   
+                        ifnull(count( r.movie_id ),0) as cnt, 
+                        ifnull(avg( r.rating ) , 0) as avg,
+                        if( f.user_id is null, 0 , 1) as is_favorite
+                        from movie m
+                        left join rating r
+                        on m.id = r.movie_id
+                        left join favorite f
+                        on f.movie_id = m.id and f.user_id = %s
+                        group by m.id
+                        order by cnt desc
+                        limit 0, 25 ;'''
+                record = (user_id, )
+                cursor = connection.cursor(dictionary=True)
+                cursor.execute(query, record)
+            
 
             result_list = cursor.fetchall()
 
