6af5a102750522b6e87ca96d45f9eecf33200fd0
blockenters <blockenters@gmail.com>
2023-01-13 11:50:56 +0900

    add

--- api server/04_s3_upload/app.py
+++ api server/04_s3_upload/app.py
@@ -3,7 +3,8 @@ from flask_jwt_extended import JWTManager
 from flask_restful import Api
 from config import Config
 from resources.image import FileUploadResource
-from resources.rekognition import ObjectDetectionResource
+from resources.posting import PostingResource
+from resources.rekognition import ObjectDetectionResource, PhotoRekognitionResource
 
 app = Flask(__name__)
 # 환경변수 셋팅
@@ -17,6 +18,8 @@ api = Api(app)
 # 경로와 리소스(API코드)를 연결한다.
 api.add_resource(FileUploadResource, '/upload')
 api.add_resource(ObjectDetectionResource, '/object_detection')
+api.add_resource(PhotoRekognitionResource, '/get_photo_label' )
+api.add_resource(PostingResource, '/posting')
 
 
 if __name__ == '__main__' :
