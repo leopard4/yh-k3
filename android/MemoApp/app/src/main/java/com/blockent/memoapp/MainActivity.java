8df720ee16fdd837ec85aabb934077a1f4bc17f3
blockenters <blockenters@gmail.com>
2023-02-10 12:01:13 +0900

    add

--- android/MemoApp/app/src/main/java/com/blockent/memoapp/MainActivity.java
+++ android/MemoApp/app/src/main/java/com/blockent/memoapp/MainActivity.java
@@ -46,6 +46,11 @@ public class MainActivity extends AppCompatActivity {
     String accessToken;
     private ProgressDialog dialog;
 
+    // 페이징 처리를 위한 변수
+    int offset = 0;
+    int limit = 7;
+    int count = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -74,12 +79,76 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                int lastPosition = ((LinearLayoutManager)recyclerView.getLayoutManager()).findLastCompletelyVisibleItemPosition();
+                int totalCount = recyclerView.getAdapter().getItemCount();
+                if(lastPosition+1 == totalCount){
+                    // 네트워크 통해서 데이터를 더 불러온다.
+                    if(count == limit){
+                        addNetworkData();
+                    }
+                }
+
+            }
+        });
 
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, AddActivity.class);
                 startActivity(intent);
+            }
+        });
+
+
+    }
+
+    private void addNetworkData() {
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        Retrofit retrofit = NetworkClient.getRetrofitClient(MainActivity.this);
+
+        MemoApi api = retrofit.create(MemoApi.class);
+
+        Call<MemoList> call = api.getMemoList("Bearer "+accessToken, offset, limit);
+
+        call.enqueue(new Callback<MemoList>() {
+            @Override
+            public void onResponse(Call<MemoList> call, Response<MemoList> response) {
+                progressBar.setVisibility(View.GONE);
+
+                if(response.isSuccessful()){
+
+                    // 정상적으로 데이터 받았으니, 리사이클러뷰에 표시
+                    MemoList memoList = response.body();
+
+                    memoArrayList.addAll(memoList.getItems());
+
+                    adapter.notifyDataSetChanged();
+
+                    // 오프셋 코드 처리
+                    count = memoList.getCount();
+                    offset = offset + count;
+
+                }else{
+                    Toast.makeText(MainActivity.this, "서버에 문제가 있습니다.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+
+            @Override
+            public void onFailure(Call<MemoList> call, Throwable t) {
+                progressBar.setVisibility(View.GONE);
             }
         });
 
@@ -100,12 +169,20 @@ public class MainActivity extends AppCompatActivity {
 
         MemoApi api = retrofit.create(MemoApi.class);
 
-        Call<MemoList> call = api.getMemoList("Bearer "+accessToken, 0, 20);
+        // 오프셋 초기화는, 함수 호출하기 전에!!
+        offset = 0;
+        count = 0;
+
+        Call<MemoList> call = api.getMemoList("Bearer "+accessToken, offset, limit);
 
         call.enqueue(new Callback<MemoList>() {
             @Override
             public void onResponse(Call<MemoList> call, Response<MemoList> response) {
                 progressBar.setVisibility(View.GONE);
+
+                // getNetworkData 함수는, 항상 처음에 데이터를 가져오는 동작이므로
+                // 초기화 코드가 필요.
+                memoArrayList.clear();
 
                 if(response.isSuccessful()){
 
@@ -116,6 +193,10 @@ public class MainActivity extends AppCompatActivity {
 
                     adapter = new MemoAdapter(MainActivity.this, memoArrayList);
                     recyclerView.setAdapter(adapter);
+
+                    // 오프셋 처리하는 코드
+                    count = memoList.getCount();
+                    offset = offset + count;
 
                 }else{
                     Toast.makeText(MainActivity.this, "서버에 문제가 있습니다.", Toast.LENGTH_SHORT).show();
